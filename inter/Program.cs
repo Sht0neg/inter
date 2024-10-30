b12e220963475d5328d73dded5f35c1357d2e633
Admin <Admin@DESKTOP-2R5E6P7>
2024-10-30 21:04:40 +0300

    Добавьте файлы проекта.

---
+++ inter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.Serialization;
+
+List<string> names = new List<string>() { "Денис", "Марк", "Даниил", "Артём"};
+foreach (var name in names) {
+    if (name.First() == 'Д') {
+        Console.WriteLine(name);
+    }
+}
+var result = from name in names where name.StartsWith('Д') select name;
+foreach (var item in result) {
+    Console.WriteLine(item);
+}
