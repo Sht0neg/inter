fd1a4fc76b24916c25183e3f495eb874f2465993
Admin <aper@gmail.com>
2024-11-22 20:32:31 +0300

    ккк

---
+++ DzLink/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace DzLink
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            List<int> ints = new List<int>();
+            Console.WriteLine("Начальный список: ");
+            for (int i = 0; i < 15; i++) {
+                ints.Add(new Random().Next(0, 100));
+                Console.WriteLine(ints[i]);
+            }
+            var result = from r in ints where r % 2 == 0 && r > 10 select r;
+            Console.WriteLine("Отсортированный список: ");
+            foreach (var r in result)
+            {
+                Console.WriteLine(r);
+            }
+        }
+    }
+}
