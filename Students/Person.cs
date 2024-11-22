e8ef458f4e0630140841059292febc1cd383f0b3
Admin <aper@gmail.com>
2024-11-22 21:09:35 +0300

    d

---
+++ Students/Person.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Students
+{
+    public class Person
+    {
+        public string Name { get; set; }
+
+        public List<int> Marks { get; set; }
+
+        public Person(string name, List<int> marks)
+        {
+            Name = name;
+            Marks = marks;
+        }
+
+        public float AverageMarks() {
+            float av = 0;
+            foreach (var mark in Marks)
+            {
+                av += mark;
+            }
+            return av/Marks.Count;
+        }
+    }
+}
---
+++ Students/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Students
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> people = new() {
+                new Person("Андрей", new() { 4, 5, 3, 2, 3, 4}),
+                new Person("Дима", new() { 3, 5, 3, 2, 3, 4, 5, 5}),
+                new Person("Вика", new() { 4, 5, 2, 2, 3, 4, 4, 5}),
+                new Person("Анастасия", new() { 4, 5, 5, 5, 2, 3, 4}),
+                new Person("Леонид", new() { 4, 5, 3, 2, 3, 4, 3, 3, 3, 3}),
+            };
+
+            var result = from r in people select $"Имя: {r.Name}, Средний балл: {Math.Ceiling(r.AverageMarks() / 0.1) * 0.1}";
+
+            foreach ( var r in result )
+            {
+                Console.WriteLine(r);
+            }
+        }
+    }
+}
